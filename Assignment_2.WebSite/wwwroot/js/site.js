e5473f3e8c40b0cab9128a421d2999817b0aac5c
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-11-08 16:07:12 +0000

    - Complete Assignment 2 - 1.iii) Animate an element of your site using Javascript. - Add JS function myMove and frame to animate a square - Add CSS code for the animation - Add HTML elements for the animation - Commented HTML, CSS and JS Code - Uploaded to GitHub

--- Assignment_2.WebSite/wwwroot/js/site.js
+++ Assignment_2.WebSite/wwwroot/js/site.js
@@ -1,4 +1,27 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+/* Advanced Software and Web Developer Diploma
+Course VIII: Visual Studio by Pitman Training / The Tech Academy
+by Steven Partlow
+
+Assigment 2: Create an ASP.NET Website that uses Git Version Control */
+
+var id = null; // Variable to store our Animation element 
+
+function myMove() {
+    var elem = document.getElementById("Animation"); // Get the Animation element from the HTML document
+    var pos = 0; // Set the position to 0
+
+    clearInterval(id); // Stop any running setInterval methods
+    id = setInterval(frame, 10); // Execute one frame of animation every 10 milliseconds
+    function frame() { // This function executes one frame of animation
+        if (pos == 350) { // Once the animation hits this position 
+            clearInterval(id); // Stop the animation
+        } else {
+            pos++; // Increment the position
+            elem.style.top = pos + 'px'; // Move the square down by one
+            elem.style.left = pos + 'px'; // Move the square left by one
+        } // End IF
+    } // End FUNCTION
+} // End FUNCTION
